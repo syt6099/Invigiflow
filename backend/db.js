6b79f552f28ccf91932391a9346c24cadaff396c
syt <146960978+syt6099@users.noreply.github.com>
2026-08-29 16:09:44 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,9 @@
 const { Pool } = require('pg');
-const fs = require('fs');
-const path = require('path');
-
-const caCert = fs.readFileSync(path.join(__dirname, 'ca.pem')).toString();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true,    // enforce certificate validation
-        ca: caCert,                  // use the CA cert from Aiven
+        rejectUnauthorized: false   // disables certificate validation
     }
 });
 
